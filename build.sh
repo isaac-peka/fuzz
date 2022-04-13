4eb46f8e7ca7fb3c01e5737bb74d9c4169cc822b
Isaac Peka <7493006+sp2ip@users.noreply.github.com>
2022-04-13 15:21:53 +0100

    Set up concurrent fuzzers

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export AFL_USE_ASAN=1
+export AFL_LLVM_LAF_ALL=1
 export PATH="/usr/lib/llvm-12/bin:$PATH"
 
 cd git
--- run.sh
+++ run.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-afl-fuzz -i in/packs -o out/ -- git/git unpack-objects --strict 
+export AFL_IMPORT_FIRST=1
 
+ROOT="$PWD"
+
+I="$ROOT/in/packs/"
+O="$ROOT/out/"
+C="$[$(grep -c ^processor /proc/cpuinfo)-1]"
+
+echo "Launching main"
+tmux new-window -n main "afl-fuzz -i $I -o $O -M main -- git/git unpack-objects --strict"
+
+opts=(
+    "-L 0"
+    "-L 0 -p explore"
+    "-L 0 -p coe"
+    "-p quad"
+    "-p fast"
+    "-p exploit"
+    "-p rare"
+    "-Z"
+)
+
+for i in $(seq 1 $C); do 
+    cd $(mktemp -d) 
+
+    git init > example.txt
+    git add .
+    git commit -m "First commit" > /dev/null
+
+    echo "Launching instance $i at $PWD"
+    tmux new-window "afl-fuzz -i $I -o $O -S s$i ${opts[i%8+1]} -- $ROOT/git/git unpack-objects --strict"
+done
+
+echo "Done!"
+cd $ROOT
