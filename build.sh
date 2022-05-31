b8005cab371b7914740f7a3acdb8c38ab8077044
Isaac Peka <7493006+sp2ip@users.noreply.github.com>
2022-05-31 09:12:26 +0100

    Set up build

--- build.sh
+++ build.sh
@@ -3,3 +3,10 @@
 export AFL_USE_ASAN=1
 export AFL_LLVM_LAF_ALL=1
 export PATH="/usr/lib/llvm-12/bin:$PATH"
+
+cd ruby
+autoconf
+mkdir build
+cd build
+AR=llvm-ar RANLIB=llvm-randlib CC=afl-lto CXX=afl-lto++ LD=afl-ld-lto ../configure
+make miniruby
